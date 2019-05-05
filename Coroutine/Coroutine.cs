c7f69fb407d6838673c2a6dc3f9901d7206f83f8
MKachi <mkachi@naver.com>
2019-05-05 20:07:49 +0900

    Add Coroutine class

---
+++ Coroutine/Coroutine.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+
+namespace Coroutine
+{
+    public struct Coroutine
+    {
+        public IEnumerator Routine { get; set; }
+        public IEnumerator Action { get; set; }
+    }
+}
