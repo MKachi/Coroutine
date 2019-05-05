60e432d427c99af062b427ddce53d2f318fbe59d
MKachi <mkachi@naver.com>
2019-05-05 20:40:58 +0900

    Add WaitForSeconds

---
+++ Coroutine/WaitForSeconds.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections;
+
+namespace Coroutine
+{
+    public class WaitForMilliSeconds : IEnumerator
+    {
+        private DateTime _targetTime;
+        private int _waitTime = 0;
+
+        public object Current { get; set; }
+
+        public WaitForMilliSeconds(int millisecond)
+        {
+            _waitTime = millisecond;
+            Reset();
+        }
+
+        public bool MoveNext()
+        {
+            if (DateTime.Now >= _targetTime)
+            {
+                return true;
+            }
+            return false;
+        }
+
+        public void Reset()
+        {
+            Current = null;
+            _targetTime = DateTime.Now.AddMilliseconds(_waitTime);
+        }
+    }
+}
